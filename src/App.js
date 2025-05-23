ebe387e8dc18fc9f915d4c3856aa85cba3f12fb6
AlexaJax365 <jmacasiano@gmail.com>
2025-05-23 13:09:59 -0400

    Fix: Add or rename App.js to match import

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>I Don’t Want to Eat That</h1>
+      <p>This app helps you figure out what you *don’t* want to eat.</p>
+    </div>
+  );
+}
+
+export default App;
