e64733fb2c3a7c0c729dd2469eafeac4599dbf29
AlexaJax365 <jmacasiano@gmail.com>
2025-06-30 22:56:52 -0400

    changed params term to restaurants (from food) in yelpCategoriesByLocation.js

--- api/yelpCategoriesByLocation.js
+++ api/yelpCategoriesByLocation.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
   } = req.query;
 
   const params = {
-    term: "food",
+    term: "restaurants",
     limit: Number(limit),
     sort_by: "distance",
   };
